5b82ee5d6337cd24b3692730842d763f36bab6cf
Demarcily <60388907+Demarcily@users.noreply.github.com>
2021-01-19 10:41:35 +0100

    Guesser
    Testing the counter I created but can only read entire words

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class Hangman {
+  public static void main(String [] args) {
+    Scanner in = new Scanner(System.in);
+    int guesses = 13;
+    boolean checker = true;
+    String answer = "Hello";
+
+    while (guesses > 0 && checker) {
+      String Answerknown = in.next();
+      if (Answerknown.equals(answer)) {
+        checker = false;
+      }
+      else {
+        guesses = guesses - 1;
+        System.out.println(guesses + " guesses left");
+      }
+    }
+
+    System.out.println("You won?");
+
+  }
 }
+
